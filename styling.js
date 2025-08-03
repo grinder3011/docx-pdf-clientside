aa90b5cb68042f0dc3b90f0dadf5c57718597edb
Zeljko <drimma@gmail.com>
2025-08-03 19:57:53 +0200

    Update styling.js

--- styling.js
+++ styling.js
@@ -1,3 +1,4 @@
+// styling.js
 export function parseRunStyles(runNode) {
   const style = {
     bold: false,
@@ -8,26 +9,13 @@ export function parseRunStyles(runNode) {
 
   const ns = 'http://schemas.openxmlformats.org/wordprocessingml/2006/main';
 
-  // Get the <w:rPr> run properties node
   const rPr = runNode.getElementsByTagNameNS(ns, 'rPr')[0];
   if (!rPr) return style;
 
-  // Check for <w:b> (bold)
-  if (rPr.getElementsByTagNameNS(ns, 'b').length > 0) {
-    style.bold = true;
-  }
+  if (rPr.getElementsByTagNameNS(ns, 'b').length > 0) style.bold = true;
+  if (rPr.getElementsByTagNameNS(ns, 'i').length > 0) style.italic = true;
+  if (rPr.getElementsByTagNameNS(ns, 'u').length > 0) style.underline = true;
 
-  // Check for <w:i> (italic)
-  if (rPr.getElementsByTagNameNS(ns, 'i').length > 0) {
-    style.italic = true;
-  }
-
-  // Check for <w:u> (underline)
-  if (rPr.getElementsByTagNameNS(ns, 'u').length > 0) {
-    style.underline = true;
-  }
-
-  // Check for color <w:color w:val="FF0000"/>
   const colorNode = rPr.getElementsByTagNameNS(ns, 'color')[0];
   if (colorNode) {
     const val = colorNode.getAttribute('w:val') || colorNode.getAttribute('val');
